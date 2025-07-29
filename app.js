44816e9af021f373c14db347b8c106d195c961ec
Joshua Stine <jstine13@ivytech.edu>
2025-07-29 02:15:47 -0400

    updates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 const Song = require("./models/song")
 const app = express()
 app.use(cors())
+app.use(express.json())
 
 
 const router = express.Router()
@@ -20,6 +21,18 @@ router.get("/songs", async(req,res) =>{
     console.log(err)
    }
 })
+// grab a sing songid
+
+router.get("/songs/:id", async (req, res)=> {
+    try{
+    const song = await Song.findById(req.params.id)
+    res.json(song)
+    }
+    catch (err){
+        res.status(400).send(err)
+    }
+})
+
 
 router.post("/songs", async(req,res) => {
     try{
@@ -32,5 +45,26 @@ router.post("/songs", async(req,res) => {
         res.status(400).send(err)
     }
 })
+
+//update is to update an existing record/resource/database entry... it uses a put request
+
+router.put("/songs/:id", async(req, res) => {
+    //first we need to find and update the song the front end wants us to update.
+    //to do this we need to request the id of the song from request
+    //then find it in the db and update it
+    try{
+        const song = req.body
+        await Song.updateOne({_id: req.params.id}, song)
+        console.log(song)
+        res.sendStatus(204)
+    }
+
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+
+
 app.use("/api", router)
 app.listen(3000)
